63ca4014a54cbcd902b5a8e11bcbe4bd41ed53cf
gabrielluizs1996 <gabrielluizs1996@gmail.com>
2021-06-11 18:08:19 -0300

    ajustando background do card

--- src/components/GlassCard/index.jsx
+++ src/components/GlassCard/index.jsx
@@ -72,12 +72,11 @@ const CardContainer = styled(motion.div)`
 
     //Glassmorphism
     background: rgba(202, 172, 255, 0.1);
-    /* box-shadow: inset 0px 0px 50px 0px rgba(202, 172, 255, 0.3); */
-    box-shadow: 10px 10px 20px -15px rgba(202, 172, 255, 0.3);
+    box-shadow: inset 0px 0px 50px 0px rgba(202, 172, 255, 0.3);
+    /* box-shadow: 10px 10px 20px -15px rgba(202, 172, 255, 0.3); */
     backdrop-filter: blur(7px);
-    border-right: 1px solid rgba(255,255,255,0.05);
-    border-bottom: 1px solid rgba(255,255,255,0.05);
-
+    border-right: 3px solid rgba(202, 172, 255, 0.3);
+    border-bottom: 2px solid rgba(202, 172, 255, 0.3);
 `
 
 export default GlassCard
