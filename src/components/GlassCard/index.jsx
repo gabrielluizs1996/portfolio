a2707d2d6fd52d08734852d3edc95586ead6c487
gabrielluizs1996 <gabrielluizs1996@gmail.com>
2021-06-11 10:59:55 -0300

    ajustando border do glasscard

--- src/components/GlassCard/index.jsx
+++ src/components/GlassCard/index.jsx
@@ -75,8 +75,8 @@ const CardContainer = styled(motion.div)`
     /* box-shadow: inset 0px 0px 50px 0px rgba(202, 172, 255, 0.3); */
     box-shadow: 10px 10px 20px -15px rgba(202, 172, 255, 0.3);
     backdrop-filter: blur(7px);
-    border-right: 1px solid rgba(255,255,255,0.2);
-    border-bottom: 1px solid 
+    border-right: 1px solid rgba(255,255,255,0.05);
+    border-bottom: 1px solid rgba(255,255,255,0.05);
 
 `
 
